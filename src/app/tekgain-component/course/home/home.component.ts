69c1f7ca11fad4d9266704768509d33d690f3b89
@svloc39 <svloc39@gmail.com>
2023-08-02 19:26:57 +0530

    added env variable

--- src/app/tekgain-component/course/home/home.component.ts
+++ src/app/tekgain-component/course/home/home.component.ts
@@ -81,7 +81,7 @@ export class HomeComponent implements OnInit {
       text: "You won't be able to revert this!",
       icon: 'warning',
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
+      confirmButtonColor: '#832561',
       cancelButtonColor: '#d33',
       confirmButtonText: 'Yes, delete it!'
     }).then((result) => {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "http://localhost:7777",
+  loginUrl: "http://localhost:9098/app",
 };
